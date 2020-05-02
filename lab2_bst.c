0438efba0147297b7d3f2f31229ab301e7754cc2
yuna <cya3559@naver.com>
2020-05-03 01:24:19 +0900

    Edited functions about visiting, deleting nodes.

--- lab2_bst.c
+++ lab2_bst.c
@@ -1,8 +1,8 @@
 /*
 *	Operating System Lab
 *	    Lab2 (Synchronization)
-*	    Student id : 
-*	    Student name : 
+*	    Student id :
+*	    Student name :
 *
 *   lab2_bst.c :
 *       - thread-safe bst code.
@@ -22,13 +22,22 @@
 /*
  * TODO
  *  Implement funtction which traverse BST in in-order
- *  
- *  @param lab2_tree *tree  : bst to print in-order. 
+ *
+ *  @param lab2_tree *tree  : bst to print in-order.
  *  @return                 : status (success or fail)
  */
-int lab2_node_print_inorder(lab2_tree *tree) {
+int lab2_node_print_inorder(lab2_tree* tree) {
     // You need to implement lab2_node_print_inorder function.
-
+    lab2_tree* subtree;
+    if (tree->root != NULL) {
+        subtree->root = tree->root->left;
+        lab2_node_print_inorder(subtree);
+        printf("The key is %d\n", subtree->root->key);
+        subtree->root = tree->root->right;
+        lab2_node_print_inorder(subtree);
+    }
+    else
+        return 1;
 }
 
 /*
@@ -61,13 +70,13 @@ lab2_node* lab2_node_create(int key) {
 */
 int lab2_node_insert(lab2_tree* tree, lab2_node* new_node) {
     lab2_node* now = tree->root;
-    
+
     // If the tree is empty, new_node becomes root of the tree.
     if (now == NULL) {
         tree->root = new_node;
         return 0;
     }
-    while (1) {     
+    while (1) {
         // It isn't possible to insert node that has duplicate key value.
         if (new_node->key == now->key) {
             printf("Duplicate key value...\n");
@@ -96,64 +105,88 @@ int lab2_node_insert(lab2_tree* tree, lab2_node* new_node) {
     return 0;
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which insert nodes from the BST in fine-garined manner.
  *
  *  @param lab2_tree *tree      : bst which you need to insert new node in fine-grained manner.
- *  @param lab2_node *new_node  : bst node which you need to insert. 
+ *  @param lab2_node *new_node  : bst node which you need to insert.
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert_fg(lab2_tree *tree, lab2_node *new_node){
-      // You need to implement lab2_node_insert_fg function.
+int lab2_node_insert_fg(lab2_tree* tree, lab2_node* new_node) {
+    // You need to implement lab2_node_insert_fg function.
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which insert nodes from the BST in coarse-garined manner.
  *
  *  @param lab2_tree *tree      : bst which you need to insert new node in coarse-grained manner.
- *  @param lab2_node *new_node  : bst node which you need to insert. 
+ *  @param lab2_node *new_node  : bst node which you need to insert.
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert_cg(lab2_tree *tree, lab2_node *new_node){
+int lab2_node_insert_cg(lab2_tree* tree, lab2_node* new_node) {
     // You need to implement lab2_node_insert_cg function.
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which remove nodes from the BST.
  *
  *  @param lab2_tree *tree  : bst tha you need to remove node from bst which contains key.
- *  @param int key          : key value that you want to delete. 
+ *  @param int key          : key value that you want to delete.
  *  @return                 : status (success or fail)
  */
-int lab2_node_remove(lab2_tree *tree, int key) {
-    // You need to implement lab2_node_remove function.
+int lab2_node_remove(lab2_tree* tree, int key) {
+    lab2_node* deletenode;
+    lab2_node* nownode = tree->root;
+    lab2_node* p_replacing, * replacing;
+    while (1) {
+        // This node deletes itself.
+        if (nownode->key == key) {
+            lab2_node_delete(nownode);
+        }
+
+        // Didn't find the node to delete yet.
+        // The key node is in rightside because the key is bigger than a key of nownode.
+        else if (key > nownode->key) {
+            nownode = nownode->right;
+        }
+
+        // The key node is in leftside because the key is smaller than a key of nownode.
+        else if (key < nownode->key) {
+            nownode = nownode->left;
+        }
+
+        // Anything matches the key.
+        if (nownode == NULL) {
+            printf("No node matches the key.\n");
+        }
+    }
 }
 
-/* 
+/*
  * TODO
  *  Implement a function which remove nodes from the BST in fine-grained manner.
  *
  *  @param lab2_tree *tree  : bst tha you need to remove node in fine-grained manner from bst which contains key.
- *  @param int key          : key value that you want to delete. 
+ *  @param int key          : key value that you want to delete.
  *  @return                 : status (success or fail)
  */
-int lab2_node_remove_fg(lab2_tree *tree, int key) {
+int lab2_node_remove_fg(lab2_tree* tree, int key) {
     // You need to implement lab2_node_remove_fg function.
 }
 
 
-/* 
+/*
  * TODO
  *  Implement a function which remove nodes from the BST in coarse-grained manner.
  *
  *  @param lab2_tree *tree  : bst tha you need to remove node in coarse-grained manner from bst which contains key.
- *  @param int key          : key value that you want to delete. 
+ *  @param int key          : key value that you want to delete.
  *  @return                 : status (success or fail)
  */
-int lab2_node_remove_cg(lab2_tree *tree, int key) {
+int lab2_node_remove_cg(lab2_tree* tree, int key) {
     // You need to implement lab2_node_remove_cg function.
 }
 
@@ -163,11 +196,12 @@ int lab2_node_remove_cg(lab2_tree *tree, int key) {
  *  Implement function which delete struct lab2_tree
  *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
  *
- *  @param lab2_tree *tree  : bst which you want to delete. 
+ *  @param lab2_tree *tree  : bst which you want to delete.
  *  @return                 : status(success or fail)
  */
-void lab2_tree_delete(lab2_tree *tree) {
+void lab2_tree_delete(lab2_tree* tree) {
     // You need to implement lab2_tree_delete function.
+
 }
 
 /*
@@ -175,10 +209,33 @@ void lab2_tree_delete(lab2_tree *tree) {
  *  Implement function which delete struct lab2_node
  *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
  *
- *  @param lab2_tree *tree  : bst node which you want to remove. 
+ *  @param lab2_tree *tree  : bst node which you want to remove.
  *  @return                 : status(success or fail)
  */
-void lab2_node_delete(lab2_node *node) {
-    // You need to implement lab2_node_delete function.
-}
+void lab2_node_delete(lab2_node* node) {
+    lab2_node* deletenode;
+    lab2_node* p_replacing, * replacing;
 
+    // Has only rightside subtree.
+    if (node->right != NULL && node->left == NULL) {
+        p_replacing = node;
+        replacing = p_replacing->right;
+        while (replacing->left != NULL) {
+            p_replacing = replacing;
+            replacing = p_replacing->left;
+        }
+        p_replacing = NULL;
+        node = replacing;
+    }
+    // Has left subtree.
+    else {
+        p_replacing = node;
+        replacing = p_replacing->left;
+        while (replacing->right != NULL) {
+            p_replacing = replacing;
+            replacing = p_replacing->right;
+        }
+        p_replacing = NULL;
+        node = replacing;
+    }
+}
