63439f8c8f1a416412cffcb68d177f0537a95c66
yuna <cya3559@naver.com>
2020-05-05 22:45:17 +0900

    Modified bst delete.

--- lab2_bst.c
+++ lab2_bst.c
@@ -289,7 +289,13 @@ int lab2_node_remove(lab2_tree* tree, int key) {
                 parent_terminal->right = NULL;
             else
                 now_node->left = NULL;
+
+            // 말단노드의 왼쪽자식은 있을경우 그거 옮겨줌.
+            if (rightTerminal_node->left != NULL)
+                parent_terminal->right = rightTerminal_node->left;
+
             // 말단 노드를 이동한다.
+
             parent_node->left = rightTerminal_node;
             rightTerminal_node->right = now_node->right;
             rightTerminal_node->left = now_node->left;
@@ -300,7 +306,8 @@ int lab2_node_remove(lab2_tree* tree, int key) {
                 parent_terminal->right = NULL;
             else
                 now_node->left = NULL;
-
+            if (rightTerminal_node->left != NULL)
+                parent_terminal->right = rightTerminal_node->left;
             parent_node->right = rightTerminal_node;
             rightTerminal_node->right = now_node->right;
             rightTerminal_node->left = now_node->left;
@@ -311,15 +318,16 @@ int lab2_node_remove(lab2_tree* tree, int key) {
                 parent_terminal->right = NULL;
             else
                 now_node->left = NULL;
+            if (rightTerminal_node->left != NULL)
+                parent_terminal->right = rightTerminal_node->left;
             tree->root = rightTerminal_node;
             rightTerminal_node->right = now_node->right;
             rightTerminal_node->left = now_node->left;
-            
+
             break;
         }
-        
-    }
 
+    }
     // nownode를 해제하고 비워주기!
     lab2_node_delete(now_node);
 }
@@ -348,6 +356,158 @@ int lab2_node_remove_fg(lab2_tree* tree, int key) {
  */
 int lab2_node_remove_cg(lab2_tree* tree, int key) {
     // You need to implement lab2_node_remove_cg function.
+    typedef enum side_is {
+        UNKNOWN,
+        LEFT_CHILD,
+        RIGHT_CHILD
+    } sideis; // 어떤 노드가 부모의 오른쪽인지, 왼쪽인지 정하기
+
+    // 락 잡기
+    pthread_mutex_lock(&(tree->root->mutex));
+    lab2_node* now_node = tree->root; // 방문하는 노드
+    lab2_node* parent_node = NULL; // 방문하는 노드의 부모 노드
+    lab2_node* key_node; // 삭제할 노드
+
+    sideis nownode_is = UNKNOWN;
+
+    // 키값 가지고있는 노드 찾기: 반복문 나가면 nownode는 입력키와 같은키 가지는거.
+    while (now_node->key != key) {
+        if (now_node == NULL) {
+            printf("[Error]No node to delete..Nothing matches the node put by parameter.\n");
+            return LAB2_FAIL;
+        }
+
+        // 찾는게 더 크다면 오른쪽으로 이동
+        if (key > now_node->key) {
+            parent_node = now_node;
+            now_node = parent_node->right;
+            nownode_is = RIGHT_CHILD;
+        }
+        // 찾는 값이 더 작다면 왼쪽으로 이동
+        else if (key < now_node->key) {
+            parent_node = now_node;
+            now_node = parent_node->left;
+            nownode_is = LEFT_CHILD;
+        }
+    }
+    // nodeSide_is가 UNKNOWN이라는 것은 찾는 노드가 트리의 루트라는 것 의미. 가장 꼭대기 노드 삭제하고싶음!!
+
+    // now는 자식이 없다..
+    if (now_node->left == NULL && now_node->right == NULL) {
+        switch (nownode_is) {
+        case LEFT_CHILD:
+            parent_node->left = NULL;
+            break;
+        case RIGHT_CHILD:
+            parent_node->right = NULL;
+            break;
+        case UNKNOWN:
+            tree->root = NULL;
+            break;
+        }
+    }
+
+    // now는 자식이 하나 있다.
+    else if (now_node->left == NULL || now_node->right == NULL) {
+        switch (nownode_is) {
+        case LEFT_CHILD:
+            // now의 왼쪽 자식이 있는것이다
+            if (now_node->left != NULL) {
+                parent_node->left = now_node->left;
+            }
+            // now의 오른쪽 자식이 있는것이다
+            else if (now_node->right != NULL) {
+                parent_node->left = now_node->right;
+            }
+            break;
+
+        case RIGHT_CHILD:
+            // now의 왼쪽 자식이 있는것이다
+            if (now_node->left != NULL) {
+                parent_node->right = now_node->left;
+            }
+            // now의 오른쪽 자식이 있는것이다
+            else if (now_node->right != NULL) {
+                parent_node->right = now_node->right;
+            }
+            break;
+
+        case UNKNOWN:
+            // tree의 루트를 삭제해야하므로 tree->root를 다음 노드로 가리키게 해야함.
+             // now의 왼쪽 자식이 있는것이다
+            if (now_node->left != NULL) {
+                tree->root = now_node->left;
+            }
+            // now의 오른쪽 자식이 있는것이다
+            else if (now_node->right != NULL) {
+                tree->root = now_node->right;
+            }
+            break;
+        }
+    }
+    // now는 자식이 둘 다 있다
+    else if (now_node->left != NULL && now_node->right != NULL) {
+        lab2_node* rightTerminal_node; // now의 왼쪽의 가장 오른쪽 노드
+        lab2_node* parent_terminal; // rightTerminal_node의 부모.
+
+        // now의 왼쪽자식의 가장 오른쪽 노드 탐색
+        rightTerminal_node = now_node->left;
+        parent_terminal = now_node;
+        while (rightTerminal_node->right != NULL) {
+            parent_terminal = rightTerminal_node;
+            rightTerminal_node = parent_terminal->right;
+        }
+
+        // 이부분이 좀 헷갈리긴 해..
+        switch (nownode_is) {
+        case LEFT_CHILD:
+            // 말단 노드와 그 부모 노드를 연결 끊는다.
+            if (parent_terminal != now_node)
+                parent_terminal->right = NULL;
+            else
+                now_node->left = NULL;
+            
+            // 말단노드의 왼쪽자식은 있을경우 그거 옮겨줌.
+            if (rightTerminal_node->left != NULL)
+                parent_terminal->right = rightTerminal_node->left;
+
+            // 말단 노드를 이동한다.
+            
+            parent_node->left = rightTerminal_node;
+            rightTerminal_node->right = now_node->right;
+            rightTerminal_node->left = now_node->left;
+            break;
+
+        case RIGHT_CHILD:
+            if (parent_terminal != now_node)
+                parent_terminal->right = NULL;
+            else
+                now_node->left = NULL;
+            if (rightTerminal_node->left != NULL)
+                parent_terminal->right = rightTerminal_node->left;
+            parent_node->right = rightTerminal_node;
+            rightTerminal_node->right = now_node->right;
+            rightTerminal_node->left = now_node->left;
+            break;
+
+        case UNKNOWN:
+            if (parent_terminal != now_node)
+                parent_terminal->right = NULL;
+            else
+                now_node->left = NULL;
+            if (rightTerminal_node->left != NULL)
+                parent_terminal->right = rightTerminal_node->left;
+            tree->root = rightTerminal_node;
+            rightTerminal_node->right = now_node->right;
+            rightTerminal_node->left = now_node->left;
+
+            break;
+        }
+
+    }
+    pthread_mutex_unlock(&(tree->root->mutex));
+    // nownode를 해제하고 비워주기!
+    lab2_node_delete(now_node);
 }
 
 
