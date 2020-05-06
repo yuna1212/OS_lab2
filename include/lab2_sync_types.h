441e3e0bbccfe60e77e5dfacb1f94af3bded345b
YangHyeeun <63303561+YangHyeeun@users.noreply.github.com>
2020-05-06 15:09:31 +0900

    Add files via upload

--- include/lab2_sync_types.h
+++ include/lab2_sync_types.h
@@ -42,6 +42,7 @@ typedef struct lab2_node {
  */
 typedef struct lab2_tree {
     struct lab2_node *root;
+    pthread_mutex_t lock;
 } lab2_tree;
 
 /* 
@@ -63,7 +64,7 @@ typedef struct thread_arg{
  * TODO
  *  You need to implement these functions. 
  */
-int lab2_node_print_inorder(lab2_tree *tree);
+int lab2_node_print_inorder(lab2_tree *tree, int number);
 lab2_tree *lab2_tree_create();
 lab2_node *lab2_node_create(int key);
 int lab2_node_insert(lab2_tree *tree, lab2_node *new_node);
